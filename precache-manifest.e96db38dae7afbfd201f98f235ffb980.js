7adffb3e7bd5083e0efdb1c8848b818002643755
Mohammed-Elshaer <maelshaer83@gmail.com>
2022-07-26 15:50:45 +0200

    Updates

---
+++ precache-manifest.e96db38dae7afbfd201f98f235ffb980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29fc698551e5c27699ca",
+    "url": "/myportofolio/static/css/main.00ee5c4c.chunk.css"
+  },
+  {
+    "revision": "29fc698551e5c27699ca",
+    "url": "/myportofolio/static/js/main.29fc6985.chunk.js"
+  },
+  {
+    "revision": "546dafc18c5050b214b1",
+    "url": "/myportofolio/static/js/1.546dafc1.chunk.js"
+  },
+  {
+    "revision": "b5a700a26a744b714d08",
+    "url": "/myportofolio/static/js/2.b5a700a2.chunk.js"
+  },
+  {
+    "revision": "8d84f4000bc48e61e1ba",
+    "url": "/myportofolio/static/js/runtime~main.8d84f400.js"
+  },
+  {
+    "revision": "1b08fc8e8cc6413f96fd6033e51fd4bd",
+    "url": "/myportofolio/index.html"
+  }
+];
